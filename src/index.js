9ac65f0fc5a07acf08f587b5201eec99ed9a1b97
davis009 <davis6247@gmail.com>
2018-03-03 04:59:57 +0300

    can't solve for N > 10000000

--- src/index.js
+++ src/index.js
@@ -1,74 +1,69 @@
 module.exports = function count(s, pairs) { //
 
 	let N = findN(pairs);
-	//s = s.split("").reverse().join("");
 
-	if(N > 1000000) return;  //can't solve yet for N > 1000000
+	if(N > 10000007) return;  //can't solve yet for N > 1000000
+													   
+	let tempMask = [], count = 0;
 
-	let resultMask = '1'.repeat(N);
-	let tempMask = [];
-
-	for(let j = 0; j < s.length; j++){		//iterate over bits in bit mask for(let j = 0; j < s.length; j++){
-		for(let k = 1; k <= N; k++){		//iterate over possible numbers
+	for(let k = 1; k <= N; k++){		
+		for(let j = 0; j < s.length; j++){		
 			if(s[j] === '1'){
 				if(gcd(k + j, N) === 1){
-					tempMask.push('1');
+					tempMask.push(1);
 				}
 				else
-					tempMask.push('0');
+					tempMask.push(0);
 
 			}
 			else if(s[j] === '0'){
 				if(gcd(k + j, N) !== 1){
-					tempMask.push('1');
+					tempMask.push(1);
 				}
 				else
-					tempMask.push('0');
+					tempMask.push(0);
 			}			
 		}
-		resultMask = logicalORInString(resultMask, tempMask.join(""));
+		if(logicalOR(tempMask)) count++;
 		tempMask = [];
 	}
 	
-	return countK(resultMask);
-}
-
-let logicalORInString = function(firstMask, secondMask){
-	let resultMask = [];
-
-	for(let i = 0; i < firstMask.length; i++){
-		resultMask.push((+firstMask[i] & +secondMask[i]) + "");
-	}
-
-	return resultMask.join("");
-}
-
-let countK = function(resultMask){
-	let count = 0;
-
-	for(let i = 0; i < resultMask.length; i++){
-		if(resultMask[i] === '1') count++;
-	}
-
 	return count;
+}
+
+let logicalOR = function(bitMaskArr){
+	let temp = 1;
+	for(let i = 0; i < bitMaskArr.length; i++){
+		temp = (temp & bitMaskArr[i]);
+	}
+
+	if(temp === 1) return true;
+
+	return false;
 }
 
 let findN = function(pairs){
 	let N = 1;
 
-	for(let i = 0; i < pairs.length; i++)
+	for(let i = 0; i < pairs.length; i++){
 		N *= Math.pow(pairs[i][0], pairs[i][1]);
+		//N = N % 1000000007;
+	}		    
 	
 	return N;
 }
 
-let gcd = function(a, b){
-	if ( ! b) {
-        return a;
+let gcd = function(a,b) {
+    a = Math.abs(a);
+    b = Math.abs(b);
+    if (b > a) {var temp = a; a = b; b = temp;}
+    while (true) {
+        if (b == 0) return a;
+        a %= b;
+        if (a == 0) return b;
+        b %= a;
     }
-
-    return gcd(b, a % b);
 }
 
-//console.log(count('0', [[3, 1], [2, 1]]));
+//console.log(count('111100101000', [[13, 1], [3, 1], [17, 1], [11, 1], [2, 1], [23, 1], [29, 1], [19, 1]]));
 
